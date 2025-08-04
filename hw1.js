3c6450c6a82fdc41fe3cb0c237e2f2399cd2c3f2
Grigoriy670 <smerte.ru@mail.ru>
2025-08-04 15:05:33 +0700

    Задание№1

---
+++ hw1.js
@@ -0,0 +1,27 @@
+//Задание 1
+let a = 10;
+alert(a);
+a = 20;
+alert(a);
+
+// //Задание 2
+
+// const dateIphone = 2007;
+// alert(dateIphone);
+
+// // Задание 3
+
+// const nameCreatorJS = 'Брендан Эйх';
+
+// alert(nameCreatorJS);
+
+// //Задание 4
+
+// let a = 10;
+// let b = 2;
+
+// console.log(a + b);
+// console.log(a - b);
+// console.log(a * b);
+// console.log(a / b);
+
