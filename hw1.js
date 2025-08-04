80d88e3b9811c646d6fc6dcbd315b49277505371
Grigoriy670 <smerte.ru@mail.ru>
2025-08-04 15:16:09 +0700

    Задание 7

--- hw1.js
+++ hw1.js
@@ -32,9 +32,22 @@
 
 //Задание 6
 
-let a = 9;
-let b = 2;
-alert(a % b);
+// let a = 9;
+// let b = 2;
+// alert(a % b);
+
+//Задание 7
+
+let num = 1;
+num = + 5;
+num = - 3;
+num = * 7;
+num = / 3;
+num = + 1;
+num = - 1;
+alert(num);
+
+
 
 
 
