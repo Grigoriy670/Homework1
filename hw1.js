cfe441f1ebd929f308163dda57be8f538734a9f4
Grigoriy670 <smerte.ru@mail.ru>
2025-08-04 15:18:41 +0700

    Задание 8

--- hw1.js
+++ hw1.js
@@ -38,15 +38,19 @@
 
 //Задание 7
 
-let num = 1;
-num = + 5;
-num = - 3;
-num = * 7;
-num = / 3;
-num = + 1;
-num = - 1;
-alert(num);
+// let num = 1;
+// num = + 5;
+// num = - 3;
+// num = * 7;
+// num = / 3;
+// num = + 1;
+// num = - 1;
+// alert(num);
 
+// Задание 8
+
+let age = prompt('Сколько вам лет?');
+alert(age);
 
 
 
