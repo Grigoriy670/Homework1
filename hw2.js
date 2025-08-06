bb57c60edffb42a85de83de15e4ecc96c5d00fb8
Grigoriy670 <smerte.ru@mail.ru>
2025-08-06 16:14:26 +0700

    №5

--- hw2.js
+++ hw2.js
@@ -35,7 +35,39 @@
 
 // #4
 
-let a = '2';
-let b = '3';
+// let a = '2';
+// let b = '3';
 
-alert ( Number(a) + Number(b));
+// alert ( Number(a) + Number(b));
+
+// #5
+
+let MonthNumber = 12
+
+switch (MonthNumber) {
+    case 1:
+    case 2:
+    case 12:   
+    alert('Зима') 
+            break;
+    case 3:
+    case 4:
+    case 5:
+        alert('Весна')
+        break; 
+     case 6:
+    case 7:
+    case 8:
+        alert('Лето')
+        break; 
+
+     case 9:
+    case 10:
+    case 11 :
+        alert('Осень')
+        break;     
+
+    default:
+         alert('Неверно')
+        break;
+}
