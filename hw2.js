d9cb30951856ee850894168aeb90d0e37a5b2798
Grigoriy670 <smerte.ru@mail.ru>
2025-08-06 15:53:34 +0700

    Задание2 = 10

--- hw2.js
+++ hw2.js
@@ -1,9 +1,22 @@
-// #1
-let password = 'password'
-let UserPas = prompt('Введите пароль')
-if(UserPas === password ) {
-    alert('Пароль введен верно')
-}
-else{
-    alert('Пароль введен неправильно')
+// // #1
+// let password = 'password'
+// let UserPas = prompt('Введите пароль')
+// if(UserPas === password ) {
+//     alert('Пароль введен верно')
+// }
+// else{
+//     alert('Пароль введен неправильно')
+// }
+
+
+// №2
+
+let c = 10
+
+if ( c >= 0 && c <= 10) {
+   alert('Верно') 
+    
+} else {
+    alert('Неверно')
+    
 }
