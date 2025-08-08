2b872edc66c09c801cce1ab151d2f043eef40e3b
Grigoriy670 <smerte.ru@mail.ru>
2025-08-08 19:11:43 +0700

    hw3

---
+++ hw3.js
@@ -0,0 +1,55 @@
+// Задание1
+
+
+// for (let i = 0 ; i < 2; i++) {
+// console.log('Привет');
+    
+// }
+
+// Задание2
+
+// for (let i = 1; i <= 5; i++) {
+//     console.log(i);
+    
+// }
+
+
+// Задание3
+
+// for (let i = 7; i <= 22; i++) {
+//         console.log(i);}
+
+// Задание4
+
+// const obj = {
+// "Коля" : '200',
+// "Вася" : '300',
+// "Петя" : '400'
+// }
+
+// for (const key in obj) {
+//     console.log(`${key} - ${obj[key]}`);
+    
+// }
+
+// Задание5
+
+// let n = 1000;
+// let num = 0;
+// while (n >= 50) {
+//  n/=2;
+//  console.log(n);   
+//  num++;
+// }
+// console.log(`Количество циклов: ${num}`) ;
+
+
+// Задание6
+
+// let dayFryday = 1;
+
+// let allDay = 31;
+// for (let i = dayFryday; i <= allDay; i += 7) {
+//     console.log(`Сегодня пятница, ${i}-е число. Необходимо подготовить отчет.`);
+    
+// }
