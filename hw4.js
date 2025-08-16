82c1fc989f052b5be623394ab7a21b3e27b08983
Grigoriy670 <smerte.ru@mail.ru>
2025-08-16 17:47:19 +0700

    задание2

--- hw4.js
+++ hw4.js
@@ -7,9 +7,25 @@
     
 // }
 
-const min = (a, b) => (a < b) ? a : b
+// const min = (a, b) => (a < b) ? a : b
 
-console.log(min(8, 4))
-console.log(min(6, 6))
+// console.log(min(8, 4))
+// console.log(min(6, 6))
+
+// function isEven(n) {
+// if (n % 2 == 0) {
+//     return 'Число четное'
+    
+// } 
+// else {
+//     return 'Число нечетное'
+    
+// }
+// }
+
+const isEven = (n) => (n % 2 === 0) ? 'Число четное' : 'Число нечетное'
+
+console.log(isEven(2))
+console.log(isEven(3)) 
 
 
