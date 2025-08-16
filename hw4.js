a912f8bfec3749c1777ead1a895d7c90e8669cd5
Grigoriy670 <smerte.ru@mail.ru>
2025-08-16 16:50:12 +0700

    Задание1

---
+++ hw4.js
@@ -0,0 +1,15 @@
+// function min(a, b) {
+//     if (a < b) {
+//         return a
+//     } else {
+//         return b
+//     }
+    
+// }
+
+const min = (a, b) => (a < b) ? a : b
+
+console.log(min(8, 4))
+console.log(min(6, 6))
+
+
