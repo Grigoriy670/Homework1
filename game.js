539d71539384b459c2fc27eaca9d6dfd7cf9e942
Grigoriy670 <smerte.ru@mail.ru>
2025-08-19 13:12:29 +0700

    Исправление игры

--- game.js
+++ game.js
@@ -1,25 +1,34 @@
 
 function guessNumberGameButton() {
-    const randomNumber = Math.floor(Math.random() * 100) + 1;
-    console.log(randomNumber);
-   
-while (true) {
-    let guess = prompt('Угадай число от 1 до 100');
-    guess = Number(guess);
-if(guess === randomNumber) {
-alert('Молодец! Поздравляю ты угадал число');
-break;
+    const maxNumber = 100;
+    const randomNumber = Math.floor(Math.random() * maxNumber) + 1; 
+    let attempts = 0;
+    
+    while (true) {
+        let guess = prompt(`Угадай число от 1 до ${maxNumber} (введите "отмена" для выхода)`);
+        
+        
+        if (guess === null || guess.toLowerCase() === 'отмена') {
+            alert('Игра отменена');
+            return;
+        }
+        
+       
+        guess = Number(guess);
+        if (isNaN(guess) || guess < 1 || guess > maxNumber) {
+            alert('Пожалуйста, введите число от 1 до 100!');
+            continue; 
+        }
+        
+        attempts++;
+        
+        if (guess === randomNumber) {
+            alert(`🎉 Поздравляю! Ты угадал число ${randomNumber} за ${attempts} попыток!`);
+            return; 
+        } else if (guess > randomNumber) {
+            alert('⬇ Меньше! Попробуй ещё раз.');
+        } else {
+            alert('⬆ Больше! Попробуй ещё раз.');
+        }
+    }
 }
-else if(guess > randomNumber ){
-   alert('Больше! Попробуй еще раз');
-   
-}    
-else {
-    alert('Меньше! Попробуй еще раз');
-   
-}
-}
-
-}
-
-
--- hw4.js
+++ hw4.js
@@ -1,80 +1,80 @@
-// function min(a, b) {
-//     if (a < b) {
-//         return a
-//     } else {
-//         return b
-//     }
+function min(a, b) {
+    if (a < b) {
+        return a
+    } else {
+        return b
+    }
     
-// }
+}
 
-// const min = (a, b) => (a < b) ? a : b
+const min = (a, b) => (a < b) ? a : b
 
-// console.log(min(8, 4))
-// console.log(min(6, 6))
+console.log(min(8, 4))
+console.log(min(6, 6))
 
-// function isEven(n) {
-// if (n % 2 == 0) {
-//     return 'Число четное'
+function isEven(n) {
+if (n % 2 == 0) {
+    return 'Число четное'
     
-// } 
-// else {
-//     return 'Число нечетное'
+} 
+else {
+    return 'Число нечетное'
     
-// }
-// }
+}
+}
 
-// const isEven = (n) => (n % 2 === 0) ? 'Число четное' : 'Число нечетное'
+const isEven = (n) => (n % 2 === 0) ? 'Число четное' : 'Число нечетное'
 
-// console.log(isEven(2))
-// console.log(isEven(3)) 
+console.log(isEven(2))
+console.log(isEven(3)) 
 
-// const up = (n) => n ** 2
+const up = (n) => n ** 2
     
-// console.log(up(5))
+console.log(up(5))
 
 
-// function age() {
+function age() {
 
-//     let age = +prompt('Сколько тебе лет?')
-//     if (age < 0) {
-//        alert ('Вы ввели не правильное значение')
-//     }  else if (age >= 0 || age <= 12) {
-//         alert('Привет, Друг!') 
-//     }
-//   else{
-//         alert('Добро пожаловать')
-// }
-// }
+    let age = +prompt('Сколько тебе лет?')
+    if (age < 0) {
+       alert ('Вы ввели не правильное значение')
+    }  else if (age >= 0 || age <= 12) {
+        alert('Привет, Друг!') 
+    }
+  else{
+        alert('Добро пожаловать')
+}
+}
 
-// age()
+age()
 
-// function calc(a, b) {
-//     console.log(isNaN(a));
-//     console.log(isNaN(b));
-//     if (isNaN(a) || isNaN(b)) {
-//         return 'Одно или оба значения не являются числом'
-//     } else {
-//         return a * b
+function calc(a, b) {
+    console.log(isNaN(a));
+    console.log(isNaN(b));
+    if (isNaN(a) || isNaN(b)) {
+        return 'Одно или оба значения не являются числом'
+    } else {
+        return a * b
         
-//     }
-// }
+    }
+}
 
-// console.log(calc(5 , 10));
+console.log(calc(5 , 10));
 
 
-// function num () {
-//     let number = prompt('Введите число')
-//     if (isNaN(number)) {
-//         return 'Переданный параметр не является числом'
+function num () {
+    let number = prompt('Введите число')
+    if (isNaN(number)) {
+        return 'Переданный параметр не является числом'
         
-//     } 
-//     else {
-//         return `${number} в кубе равняется ${number ** 3}`
+    } 
+    else {
+        return `${number} в кубе равняется ${number ** 3}`
         
-//     }
-// }
+    }
+}
 
-// console.log(num());
+console.log(num());
 
 function getArea(){
     return Math.PI * this.radius ** 2
