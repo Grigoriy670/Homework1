4a53b082b4ed5a70ce6c9e79bce05fdcf785cb55
Grigoriy670 <smerte.ru@mail.ru>
2025-08-21 19:29:20 +0700

    Задание4

--- hw5.js
+++ hw5.js
@@ -1,14 +1,27 @@
-const arr = [1, 5, 4, 10, 0, 3];
-for(let i = 0; i < arr.length; i++) {
-    console.log(arr[i]);
-    if (arr[1] === 10) {
-        break;
+// const arr = [1, 5, 4, 10, 0, 3];
+// for(let i = 0; i < arr.length; i++) {
+//     console.log(arr[i]);
+//     if (arr[1] === 10) {
+//         break;
+//     }
+// }
+
+// const arr = [1, 5, 4, 10, 0, 3];
+// console.log( arr.indexOf(4) );
+
+
+// const arr = [1, 3, 5, 10, 20];
+// console.log(arr.join(" "));
+
+const arr = [] ;
+
+
+for (let i = 0; i < 3; i++) {
+    const arr2 = [];
+    for(let i = 0; i < 3; i++){
+        arr2.push(1);
     }
+    arr.push(arr2)
 }
 
-const arr = [1, 5, 4, 10, 0, 3];
-console.log( arr.indexOf(4) );
-
-
-const arr = [1, 3, 5, 10, 20];
-console.log(arr.join(" "));
+console.log(arr);
