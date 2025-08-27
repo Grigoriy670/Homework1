1611438b7ec37cde9fe052325802fa834859cf27
Grigoriy670 <smerte.ru@mail.ru>
2025-08-27 09:57:27 +0700

    Задание10

--- hw6.js
+++ hw6.js
@@ -54,4 +54,20 @@ currentDate.setDate(currentDate.getDate() + 73);
 console.log(currentDate);
 
 
+function formatDate(date) {
+    const days = ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'];
+    const months = ['Январь', 'февравль', 'март', 'апрель', 'май', 'июнь', 'июль', 'август', 'сентябрь', 'октябрь', 'ноябрь', 'декабрь']
+    const year = date.getFullYear();
+    const day = date.getDate();
+    const month = date.getMonth();
+    const dayOfWeek = days[date.getDay()]
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
+    const seconds = date.getSeconds()
 
+    return `
+    Дата: ${day} ${months[month]} ${year} - это ${dayOfWeek}
+    Время: ${hours} ${minutes} ${seconds}`;
+}
+
+console.log(formatDate(new Date()));
