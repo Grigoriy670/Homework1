a6efa8d11d1a6761c4bd99ffc5b8f2f67aaaf73b
Grigoriy670 <smerte.ru@mail.ru>
2025-08-27 09:11:59 +0700

    Задание2

--- hw6.js
+++ hw6.js
@@ -1,3 +1,13 @@
 const strings = 'js'
 console.log(strings.toUpperCase());
 
+function filter(arr,str) {
+    let result = [];
+    for (let i = 0; i < arr.length; i++) {
+        if(arr[i].toLowerCase().startsWith(str.toLowerCase())){
+        result.push(arr[i])
+    }
+}
+    return result;
+}
+console.log(filter(['JavaScript', 'Python', 'Java'],'ja'));
