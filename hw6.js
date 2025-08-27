a06598a9fc2d8bf90db2dec44a405186f1521a44
Grigoriy670 <smerte.ru@mail.ru>
2025-08-27 09:23:40 +0700

    Задание 4-5

--- hw6.js
+++ hw6.js
@@ -18,3 +18,16 @@ const roundNumber = 32.58884;
 console.log(Math.floor(roundNumber));
 console.log(Math.round(roundNumber));
 console.log(Math.ceil(roundNumber));
+
+
+console.log(Math.max(52, 53, 49, 77, 21, 32));
+console.log(Math.min(52, 53, 49, 77, 21, 32))
+
+
+function getRandomsNumber() {
+    return Math.floor(Math.random() * 10) + 1;
+}
+
+console.log(getRandomsNumber());
+
+
