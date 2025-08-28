9e24504b81cd4855c44258c920086c02efa06fa7
Grigoriy670 <smerte.ru@mail.ru>
2025-08-28 09:44:02 +0700

    Задание2

--- hw7.js
+++ hw7.js
@@ -10,3 +10,30 @@ return a.age - b.age
 })
 
 console.log(people);
+
+function isPositive(num) {
+    return num > 0;
+}
+function isMale(person) {
+    return person.gender === 'male'
+}
+function filter( array, ruleFunction) {
+    const result = [];
+    for (let i = 0; i < array.length; i++) {
+      if (ruleFunction(array[i])) {
+        result.push(array[i]);
+      }
+        
+    } return result
+}  
+
+console.log(filter([3, -4, 1, 9], isPositive));
+
+const peoples = [
+   {name: 'Глеб', gender: 'male'},
+   {name: 'Анна', gender: 'female'},
+   {name: 'Олег', gender: 'male'},
+   {name: 'Оксана', gender: 'female'}
+];
+
+console.log(filter(peoples, isMale));
